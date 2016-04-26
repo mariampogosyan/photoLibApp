c2429e9f2404b8503d42fa95f896929bbdf6dc20
Mariam Pogosyan <vmariam.pogosyan@gmail.com>
2016-04-26 13:49:26 -0400

    photoclass

--- app/src/main/java/com/example/masha/photoalbumapp52/Photo.java
+++ app/src/main/java/com/example/masha/photoalbumapp52/Photo.java
@@ -4,4 +4,32 @@ package com.example.masha.photoalbumapp52;
  * Created by Mariam on 4/24/2016.
  */
 public class Photo {
+    private String photoName;
+    private String fileURL;
+    private int id;
+
+    public Photo(){
+        this(null,null);
+    }
+
+    public Photo(String photoName, String fileURL){
+        this.photoName = photoName;
+        this.fileURL = fileURL;
+    }
+
+    public String getPhotoName() {
+        return photoName;
+    }
+
+    public void setPhotoName(String photoName) {
+        this.photoName = photoName;
+    }
+
+    public String getFileURL() {
+        return fileURL;
+    }
+
+    public void setFileURL(String fileURL) {
+        this.fileURL = fileURL;
+    }
 }
